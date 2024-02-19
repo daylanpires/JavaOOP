66b7fa37b69bb495ba9074172416240241cc4ebd
Daylan Pires <daylanpires@gmail.com>
2024-02-19 00:57:16 -0300

    data processing

--- src/javaOOP/dataProcessing3.java
+++ src/javaOOP/dataProcessing3.java
@@ -3,14 +3,16 @@ package javaOOP;
 public class dataProcessing3 {
 
 	public static void main(String[] args) {
-		
+		//Casting is the conversion of objects or primitive types from one type to another type.
+		//It should be noted that the casting of primitive types is applicable to all, 
+		//except the boolean type (for which, you cannot cast a boolean to any other type and vice versa).
 		int a, b;
 		double resultado;
 		
 		a = 5;
 		b = 2;
 		
-		resultado = (double) a/b;
+		resultado = (double) a / b;
 		
 		System.out.println(resultado);
 
